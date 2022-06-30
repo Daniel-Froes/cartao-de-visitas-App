8f8a0e955a758eddc5dbf41507b8fbeb73e2d240
Daniel Froes <danielfroestbf@gmail.com>
2022-06-30 13:05:28 -0300

    feat: configurando o material card e arrumando o recycler view

---
+++ BusinessCard/app/src/main/java/me/daniel/businesscard/App.kt
@@ -0,0 +1,6 @@
+package me.daniel.businesscard
+
+import android.app.Application
+
+class App: Application() {
+}
--- BusinessCard/app/src/main/java/me/daniel/businesscard/ui/MainActivity.kt
+++ BusinessCard/app/src/main/java/me/daniel/businesscard/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package me.daniel.businesscard.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.daniel.businesscard.R
+import me.daniel.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
